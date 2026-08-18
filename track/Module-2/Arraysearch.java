99aa2e817872efb11bbe9f9de0caa0769edffe7c
ksusmithareddy30 <kempireddygarisusmithareddy@gmail.com>
2026-08-18 11:57:08 +0530

    Day7

---
+++ track/Module-2/Arraysearch.java
@@ -0,0 +1,31 @@
+class Arraysearch 
+{
+    void search()
+    {
+    void search(int ref[].int key)
+    {
+        boolean found = true;
+        for (int i = 0; i<=ref.length-1; i++) 
+        {
+           if(ref[i]==key) 
+        {
+            System.out.println("Element found at index :"+i);
+            found=false;
+            break;
+        } 
+           if(found)
+        {
+            System.out.println("Element not found");
+        }
+        }
+    }
+    }
+    class Main {
+    public static void main(String[] args)
+    {
+        int arr[]={10,20,30,40,50};
+        ArraySearch as = new ArraySearch();
+        as.search(arr,30);
+    }
+}
+}
